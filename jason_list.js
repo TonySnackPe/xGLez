2560978fa2d19e93ff2356aa191b50cb16d105ef
TONY <130157657+TonySnackPe@users.noreply.github.com>
2024-07-13 23:20:44 -0500

    Add files via upload

---
+++ jason_list.js
@@ -0,0 +1,51 @@
+
+var friends = [
+    {
+        name: 'My Love ',
+        user: 'My Love ',
+        url: 'iSrCriss',
+        img: 'https://i.postimg.cc/8cXVh7bm/maah.jpg'
+    },
+    {
+        name: 'SoolG',
+        user: 'SoolG',
+        url: 'xSoolG',
+        img: 'https://i.postimg.cc/YqDggcSk/eloh.jpg'
+    },
+    {
+        name: 'Pitu',
+        user: 'Pitu',
+        url: 'xPiitu',
+        img: 'https://i.postimg.cc/dVzyH8dx/paula.jpg'
+    },
+    {
+        name: 'Mont',
+        user: 'Mont',
+        url: 'xMont',
+        img: 'https://i.postimg.cc/y6P0134D/anneh.jpg'
+    },
+    {
+        name: 'Teki',
+        user: 'Teki',
+        url: 'Teki',
+        img: 'https://i.postimg.cc/Pxt9f27f/and-img.jpg'
+    },
+    {
+        name: 'Luna',
+        user: 'Luna',
+        url: 'LDuLzura25',
+        img: 'https://i.postimg.cc/7ZCznBCz/misty.jpg'
+    },
+    {
+        name: 'Gaston',
+        user: 'Gaston',
+        url: 'iGaston',
+        img: 'https://i.postimg.cc/8CJYDWtW/dk.jpg'
+    },
+    {
+        name: 'Jack',
+        user: 'Jack ',
+        url: 'Jackk',
+        img: 'https://i.postimg.cc/VNtbVn7c/franciny.jpg'
+    }
+];
